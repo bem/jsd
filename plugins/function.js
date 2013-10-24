d3520ce2d711f2b1f0e2807cf511a0f724f95a4f
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-10-24 17:21:33 +0400

    add export support

--- plugins/function.js
+++ plugins/function.js
@@ -1,10 +1,23 @@
 module.exports = function(jsdoc) {
     jsdoc
         .registerBuilder(function(tag, jsdocNode, astNode) {
-            if(tag.isFirst && astNode.type === 'Property' && astNode.value.type === 'FunctionExpression') {
-                var functionNode = { type : 'function', name :  astNode.key.value || astNode.key.name };
-                (jsdocNode.fields || (jsdocNode.fields = [])).push(functionNode);
-                return functionNode;
+            if(tag.isFirst) {
+                if(astNode.type === 'Property' && astNode.value.type === 'FunctionExpression') {
+                    var functionNode = { type : 'function', name :  astNode.key.value || astNode.key.name };
+                    (jsdocNode.fields || (jsdocNode.fields = [])).push(functionNode);
+                    return functionNode;
+                }
+                else if(astNode.type === 'FunctionExpression') {
+                    var functionNode = { type : 'function' };
+                    if(jsdocNode.type === 'module') {
+                        jsdocNode.exports = functionNode;
+                    }
+                    else {
+                        throw Error('Unexpected function expression');
+                    }
+
+                    return functionNode;
+                }
             }
         });
 };
--- plugins/module.js
+++ plugins/module.js
@@ -3,7 +3,10 @@ module.exports = function(jsdoc) {
         .registerTag('module', function(comment) {
             return { name : comment };
         })
-        .registerBuilder(function(tag, jsdocNode) {
+        .registerTag('exports', function(comment) {
+            return { name : comment };
+        })
+        .registerBuilder(function(tag, jsdocNode, astNode) {
             switch(tag.type) {
                 case 'module':
                     var moduleNode = { type : 'module', name : tag.name };
@@ -11,12 +14,23 @@ module.exports = function(jsdoc) {
                         (this.modules || (this.modules = {}))[tag.name] = moduleNode);
                     return moduleNode;
 
+                case 'exports':
+                    var module = this.modules[tag.name];
+                    if(astNode.type === 'FunctionExpression') {
+                        return module;
+                    }
+                    else {
+                        return module.exports || (module.exports = { type : 'object', fields : [] });
+                    }
+
                 case 'alias':
                     var matches = tag.to.split(':');
                     if(matches.length === 2) {
                         var module = this.modules[matches[0]];
-                        (module.exports || (module.exports = [])).push(
-                            { type : 'export', name : matches[1], content : jsdocNode });
+                        (module.exports || (module.exports = {
+                            type : 'object',
+                            fields : []
+                        })).fields.push(jsdocNode);
                     }
                 break;
             }
