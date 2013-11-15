80eb844e081af5d3cf3b1ce7447d10b107a1b476
Sergey Berezhnoy <veged@yandex-team.ru>
2013-11-15 18:32:46 +0400

    Throw error on @exports to unknown module

--- plugins/exports.js
+++ plugins/exports.js
@@ -5,6 +5,7 @@ module.exports = function(jsdoc) {
         })
         .registerBuilder('exports', function(tag, curJsdocNode) {
             var module = this.modules[tag.name];
+            if(!module) throw Error('Unknown module: ' + tag.name);
             return module.exports || (module.exports = curJsdocNode);
         })
         .registerBuilder('alias', function(tag, curJsdocNode) {
