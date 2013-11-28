703c19bec9362f4fc313cbce14c357daf0e5d395
Sergey Berezhnoy <veged@yandex-team.ru>
2013-11-28 16:23:42 +0400

    @event: Parse description from tag (close #55)

--- plugins/event.js
+++ plugins/event.js
@@ -1,10 +1,15 @@
 module.exports = function(jsdoc) {
     jsdoc
         .registerParser('event', function(comment) {
-            return { name : comment };
+            var matches = comment.match(/^(\w+)(?:\s+(.*?))?\s*$/);
+            return { name : matches[1], description: matches[2] };
         })
         .registerBuilder('event', function(tag) {
-            var eventNode = { jsdocType : 'event', name : tag.name },
+            var eventNode = {
+                    jsdocType : 'event',
+                    name : tag.name,
+                    description : tag.description
+                },
                 curClass = this.currentClass;
             (curClass.events || (curClass.events = [])).push(eventNode);
             return eventNode;
