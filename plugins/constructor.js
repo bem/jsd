e21799c55072df02300bdfd7a0566d7df6075e27
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-12-06 23:21:10 +0400

    @constructor: move constructor to `cons` property from `proto`

--- plugins/constructor.js
+++ plugins/constructor.js
@@ -4,6 +4,17 @@ module.exports = function(jsdoc) {
             return {};
         })
         .registerBuilder('constructor', function(tag, curJsdocNode) {
-            curJsdocNode.isConstructor = true;
+            this.currentClass.cons = curJsdocNode;
+
+            // we need to remove constructor from prototype if it is within
+            var props = this.currentClass.proto.props,
+                prop, i = 0;
+            while(prop = props[i]) {
+                if(prop.val === curJsdocNode) {
+                    props.splice(i, 1);
+                    break;
+                }
+                i++;
+            }
         });
 };
