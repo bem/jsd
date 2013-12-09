5f8d090b842ff15ad9751fad6bf5947aae7151af
Dmitry Filatov <dfilatov@yandex-team.ru>
2013-12-09 20:03:45 +0400

    @example: allow to make multiple examples (close #66)

--- test/example/example.js
+++ test/example/example.js
@@ -9,6 +9,8 @@ provide(/** @exports my-module */{
      * Description of method1
      * @example
      * module.method1() -- with default param p
+     *
+     * @example
      * module.method1(p) -- with given param p
      */
     method1 : function() {
