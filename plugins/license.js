b85f805ef97315c167a058aa59c809387614a3ab
dfilatov <dfilatov@yandex-team.ru>
2014-01-23 17:54:45 +0400

    remove trash

--- plugins/license.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(jsdoc) {
-    jsdoc
-        .registerParser('license', String)
-        .registerBuilder('license', function(tag, curJsdocNode) {
-            curJsdocNode.license = tag.content;
-        });
-};
